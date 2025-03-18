7f4f27e1233634fe469fb9b786e6847fb7a3e614
Suraj Chandbhanani <chandbhanani.suraj@gmail.com>
2025-03-18 14:47:23 +0530

    Configured for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Incident-Management-Dashboard/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
-    base: '/Incident-Management-Dashboard/', 
+    
   },
 });
